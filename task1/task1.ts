968d7546533ba2438300482f99c9c9743c95a8cb
Nazariy <1234567890qwerty12321@gmail.com>
2023-03-08 00:06:20 +0200

    task 2

--- task1/task1.ts
+++ task1/task1.ts
@@ -1,1 +1,17 @@
-console.log(1);
+function sumOfIntervals(ins: [number, number][]) {
+    ins.sort((a, b) => a[0] - b[0]);
+    let interval : number = 0
+    for (let i = 0; i < ins.length - 1; i++) {
+        if (ins[i][1] >= ins[i+1][0]) {
+            ins[i+1][0] = ins[i][0]
+            if (ins[i][1] >= ins[i+1][1]) {
+                ins[i+1][1] = ins[i][1]
+            }
+            delete ins[i]
+        }
+    }
+    ins.forEach(el => {if (el !== undefined) {interval += el[1] - el[0]}})
+    return interval
+}
+
+console.log(sumOfIntervals([[1, 4], [7, 10], [3, 5]]))
