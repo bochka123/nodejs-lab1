8928f63009555f7a3553d1e14468858c23d93e12
Nazariy <1234567890qwerty12321@gmail.com>
2023-03-08 00:03:25 +0200

    ended task1, deleted task1.js

--- task1/task1.js
+++
@@ -1,6 +0,0 @@
-export function sumOfIntervals(intervals: [number, number][]) {
-    let interval : number
-    intervals.forEach(element => interval += element[1] - element[0])
-}
-
-sumOfIntervals([[1, 5]])
